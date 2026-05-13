0f2f6fd14f52ce2b8125ccac8c322b6d5127c33a
wagner machado <wagnermachado52020@gmail.com>
2026-05-13 15:44:21 -0300

    Delete sw.js

--- sw.js
+++
@@ -1,8 +0,0 @@
-self.addEventListener('install', (e) => {
-  console.log('Service Worker instalado!');
-});
-
-self.addEventListener('fetch', (e) => {
-  // Isso permite que o app funcione mesmo sem internet após o primeiro acesso
-  e.respondWith(fetch(e.request));
-});
