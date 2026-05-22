9273e5b4487cd2811629ea925c8e1d32bf1c0d59
wagner machado <wagnermachado52020@gmail.com>
2026-05-22 17:04:18 -0300

    Add files via upload

---
+++ anticopia.js
@@ -0,0 +1,11 @@
+/* =========================================================
+   [INÍCIO] -SRP SEGURANÇA E PROTEÇÃO
+   ================================
+   falso:false : verdadeiro: true 
+   ========================= */
+const CONFIG_PROTECAO = {
+    bloquearCliqueDireito: false,
+    bloquearTeclado: true,
+    //seleção com dedo✓
+    bloquearSelecao: false,
+};
