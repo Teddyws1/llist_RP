fcd28dc93d67476d56f1fa8e5c3ee4980097adc1
wagner machado <wagnermachado52020@gmail.com>
2026-06-28 22:59:41 -0300

    Update proximaAtualizacao date in informacao.js

--- informacao.js
+++ informacao.js
@@ -11,7 +11,7 @@
     // ALTERE APENAS ESTAS DUAS DATAS
     ultimaAtualizacao: "23/06/2026 •18:24",
     //atualiza data de atualização 
-    proximaAtualizacao: "",
+    proximaAtualizacao: "29/06/2026",
 
     plataforma: "Android • iPhone • Windows • macOS • Linux • Web",
     
